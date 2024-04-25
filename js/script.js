4bc97758a7caed8719a7d69f7f683f5b20dc3ace
Nadia Astriani <nadiaastriani5@gmail.com>
2024-04-25 20:48:45 +0700

    add converter feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 // Ini File js
+
+let isReverse = true;
+
+function reverse () {
+    let cToF = document.getElementById('c-to-f');
+    let fToC = document.getElementById('f-to-c');
+
+    if (isReverse) {
+        console.log(isReverse);
+        fToC.style.display = "block";
+        cToF.style.display = "none";
+        isReverse = false;
+    } else {
+        // Ini Tampilan Default
+        console.log(isReverse);
+        cToF.style.display = "block";
+        fToC.style.display = "none";
+        isReverse = true;
+    }
+}
+
+function convert() {
+
+}
+
+document.getElementById("button-convert").addEventListener('click', () => convert());
